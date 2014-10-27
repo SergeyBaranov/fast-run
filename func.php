c8a3c7c27f35695fe6b105114011bd9ff9c6f8de
SergeyBaranov <workinggrid@gmail.com>
2014-10-27 21:33:21 +0400

    new files

---
+++ func.php
@@ -0,0 +1,9 @@
+<?php
+    function printInfo($info)
+    {
+            $data = "<label>Name:</label>" . "<strong>" . $info["name"] . "</strong>" .
+                    "<br>" . 
+                    "<label>Age:<label>" .
+                    "span>" . $info["age"] . "</span>";
+            echo $data;
+    }
